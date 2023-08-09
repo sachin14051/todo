3ff1d80fa1790839674985a602152ac957e2622c
sachin <sachin14051@gmail.com>
2023-08-09 23:30:04 +0530

    working on todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './shared/components/todolist/todolist.component';
+import { TodoformComponent } from './shared/components/todoform/todoform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TodoformComponent
   ],
   imports: [
     BrowserModule
